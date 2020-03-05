2bf0ed5e261a2d25af4dfe49abdac101ffb62bb5
ww-rm <774039058@qq.com>
2020-03-05 14:01:21 +0800

    perceptron

--- perceptron.py
+++ perceptron.py
@@ -1,69 +1,74 @@
 import numpy as np
 
-# mat的行数为实例特征个数, 列数为实例总数
+# ndarray的行数为实例特征个数, 列数为实例总数
 
 
 def perceptron_base(train_x, train_y, yita):
     """感知机的原始算法
 
     参数: 
-        x: np.mat类型, 行数为特征数, 列数为样本数
+        x: np.ndarray类型, 行数为特征数, 列数为样本数
         y: 类型格式与x相同, 值取+1或-1
         yita: 学习率, n取小于等于1的正数
 
     返回:
-        二元组(w, b), 均为np.mat类型
-        w为特征向量矩阵
+        二元组(w, b, f)
+        w为一维特征向量
         b为偏置常数
+        f是模型函数
     """
 
-    train_x = np.mat(train_x)
-    train_y = np.mat(train_y)
+    train_x = np.array(train_x, dtype=float)
+    train_y = np.array(train_y, dtype=float)
 
     # 获得特征数与样本数
     feature_num = train_x.shape[0]
     total_num = train_x.shape[1]
 
     #w, b初始化为0
-    w = np.zeros((1, feature_num))
-    b = np.zeros((1, 1))
+    w = np.zeros((feature_num, ))
+    w = np.array([-7., 1.])
+    b = 0.0
 
     # 选取一个实例, index是当前的实例下标
     index = 0
     count = 0
     while index < total_num:
-        if train_y[..., index] * w @ train_x[..., index] + b <= 0:
-            # print(index, end=', ')
-            w += yita*train_y[..., index]*train_x[..., index].T
-            b += yita*train_y[..., index]
+        if train_y[index] * (w @ train_x[..., index] + b) <= 0:
+            w += yita*train_y[index]*train_x[..., index].T
+            b += yita*train_y[index]
             index = 0
         else:
             index += 1
 
         # 防止不收敛
         count += 1
-        if count > 1000000:
-            print("感知机迭代次数超过1000000次")
+        if count % 10000000 == 0:
+            print("感知机迭代次数超过10000000次")
 
-    return (w, b)
+    def f(x):
+        return np.sign(w @ x + b)
+
+    return (w, b, f)
 
 
 def perceptron_dual(train_x, train_y, yita):
     """感知机的对偶算法
 
     参数: 
-        x: np.mat类型, 行数为特征数, 列数为样本数
+        x: np.array类型, 行数为特征数, 列数为样本数
         y: 类型格式与x相同, 值取+1或-1
         yita: 学习率, n取小于等于1的正数
 
     返回:
-        二元组(a, b), 均为np.mat类型
+        二元组(a, b, f)
         a为...
         b为偏置常数
+        f是模型函数
     """
 
-    train_x = np.mat(train_x)
-    train_y = np.mat(train_y)
+    train_x = np.array(train_x, dtype=float)
+    train_y = np.array(train_y, dtype=float)
 
     # 获得特征数与样本数
     # feature_num = train_x.shape[0]
@@ -73,24 +78,27 @@ def perceptron_dual(train_x, train_y, yita):
     gram_table = train_x.T @ train_x
 
     #a, b初始化为0
-    a = np.zeros((1, total_num))
-    b = np.zeros((1, 1))
+    a = np.zeros((total_num, ))
+    b = 0.0
 
     # 选取一个实例, index是当前的实例下标
     index = 0
     count = 0
     while index < total_num:
-        if train_y[..., index] @ np.multiply(a, train_y) @ gram_table[..., index] + b <= 0:
+        if train_y[index] * (np.multiply(a, train_y) @ gram_table[..., index] + b) <= 0:
             # print(index, end=', ')
             a[..., index] += yita
-            b += yita*train_y[..., index]
+            b += yita*train_y[index]
             index = 0
         else:
             index += 1
 
         # 防止不收敛
         count += 1
-        if count > 1000000:
-            print("感知机迭代次数超过1000000次")
+        if count % 10000000 == 0:
+            print("感知机迭代次数超过10000000次")
 
-    return (a.T, b)
+    def f(x):
+        return np.sign(np.multiply(a.T, train_y) @ train_x.T @ x + b)
+
+    return (a.T, b, f)
--- test.py
+++ test.py
@@ -1,24 +1,42 @@
 from perceptron import *
 import numpy as np
 import random
+import matplotlib.pyplot as plt
 
-train_x = np.mat(
-    [[1, 1],
-     [2, 1],
-     [1, 2],
-     [5, 5],
-     [6, 5],
-     [5, 6]]
-).T
+N = 50
+bias = 0
 
-train_y = np.mat(
-    [[1,1,1,-1,-1,-1]]
+train_x = np.concatenate(
+    (np.random.rand(2, N)*-10-1, np.random.rand(2, N)*(10)+1), 1)
+
+train_y = np.array(
+    [1] * (N+bias) + [-1] * (N-bias)
 )
 
 
-w, b = perceptron_base(train_x, train_y, 0.1)
-print(w,b)
+result_base = perceptron_base(train_x, train_y, 1e-5)
+result_dual = perceptron_dual(train_x, train_y, 10)
+# print(result_base)
+# print(result_dual)
+# print(np.multiply(result_dual[0].T, train_y)@train_x.T)
 
-a, b = perceptron_dual(train_x, train_y, 0.1)
-print(a,b)
-print(np.multiply(a.T, train_y)@train_x.T)
+
+def f1(x):
+    w = result_base[0]
+    b = result_base[1]
+    return -(w[0]*x+b)/w[1]
+
+
+def f2(x):
+    w = np.multiply(result_dual[0].T, train_y)@train_x.T
+    b = result_dual[1]
+    return -(w[0]*x+b)/w[1]
+
+
+line_x = [-10, 10]
+plt.plot(line_x, list(map(f1, line_x)), color='yellow')
+plt.plot(line_x, list(map(f2, line_x)), color='green')
+plt.scatter(train_x[0][:N+bias], train_x[1][:N+bias], color='blue')
+plt.scatter(train_x[0][N+bias:], train_x[1][N+bias:], color='red')
+
+plt.show()
