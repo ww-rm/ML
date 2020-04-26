59a21d657756558741eed0d2d69861b23d73acbf
ww-rm <ww-rm@qq.com>
2020-04-26 20:22:43 +0800

    add logistic

--- algorithm/logistic.py
+++ algorithm/logistic.py
@@ -1,15 +1,19 @@
 import numpy as np
 
+
 class Logistic:
     def __init__(self):
         self.total_num = None
         self.feature_num = None
 
-    def fit(self, train_x, train_y, yita=0.01):
+    def _sigmoid(self, x):
+        return 1.0/(1+np.exp(-x))
+
+    def fit(self, train_x, train_y, yita=0.0001):
         """
-        参数: 
+        参数:
             x: np.ndarray类型, 行数为特征数, 列数为样本数
-            y: 类型格式与x相同, 值取+1或-1
+            y: 类型格式与x相同, 值取+1或0
             yita: 学习率, n取小于等于1的正数
         """
 
@@ -20,28 +24,47 @@ class Logistic:
         self.feature_num = train_x.shape[0]
         self.total_num = train_x.shape[1]
 
-        #w, b初始化为0
-        self.w = np.zeros((self.feature_num, ))
-        self.b = 0.0
+        # w合并b之后, 初始化为0
+        self.w = np.zeros((self.feature_num + 1, ))
 
-        # 选取一个实例, index是当前的实例下标
-        index = 0
-        count = 0
-        while index < self.total_num:
-            if train_y[index] * (self.w @ train_x[..., index] + self.b) <= 0:
-                self.w += yita*train_y[index]*train_x[..., index].T
-                self.b += yita*train_y[index]
-                index = 0
-            else:
-                index += 1
+        # 在x样本特征的末尾加入一个常数项1
+        train_x = np.concatenate((train_x, np.ones((1, self.total_num))))
 
-            # 防止不收敛
-            count += 1
-            if count % 10000000 == 0:
-                print("感知机迭代次数超过10000000次")
+        # print(train_x)
+        # print(train_y)
+
+        # 迭代一段时间
+        flag = True
+        while flag:
+            # print(self.w)
+            flag = False
+            h = self._sigmoid(self.w@train_x)
+            error = train_y - h
+            new_w = self.w + yita*train_x@error
+
+            # 迭代到一定的精度之后退出
+            for num in (new_w-self.w).flat:
+                if np.fabs(num) > 1e-6:
+                    self.w = new_w
+                    flag = True
+                    break
+            
+            
+
+        self.b = self.w[-1]
+        self.w = self.w[0:-1]
 
         return True
 
     def predict(self, x):
+        """
+        返回值:
+            二元组(y, prob)
+            第一个值为概率大于0.5的分类结果
+            第二个值为概率值
+        """
+
         x = np.array(x)
-        return np.sign(self.w @ x + self.b)
+        # print(x, self.w, self.b)
+        prob_0 = 1.0/(1+np.exp(self.w @ x + self.b))
+        return (0, prob_0) if prob_0 > 0.5 else (1, 1-prob_0)
--- test.py
+++ test.py
@@ -2,8 +2,7 @@ import numpy as np
 import random
 import matplotlib.pyplot as plt
 
-print(dir(np.array([1,2,3])))
-
+np.
 x = np.array(
     [[1, 2],
      [3, 4],
@@ -14,9 +13,8 @@ y = np.array(
     [1, 10, 2]
 ).reshape(1, 3)
 
-z = np.concatenate((x, y)).T
-p = np.array(x)
-p[0][0] = 111
+z = np.concatenate((x, y), 0)
+np.
+print(x, y)
+print(np.exp(z))
 
-np.c
-print(x, p)
